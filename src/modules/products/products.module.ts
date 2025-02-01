96eea308015dd1f92b0b790bc375be51ff9778ca
Borisresonance <llorentejuandiego@gmail.com>
2025-02-01 00:03:36 -0500

    changes on file structure

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './application/product.service';
-import { ProductController } from './infraestructure/product/product.controller';
+import { ProductController } from './infraestructure/product.controller';
 
 @Module({
   providers: [ProductService],
